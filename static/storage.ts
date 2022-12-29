21efb53c38f83acd1ab4ecf4cc7511fdfe542cb0
Tim Hinderliter <tim@disorder.net>
2022-12-29 13:13:36 -0800

    add storage & util.ts

--- static/storage.ts
+++ static/storage.ts
@@ -1,1 +1,48 @@
-../../storage.ts
+
+
+class StoredVal<T> {
+    key: string
+    _storage: T
+    _convertForSave: ((storage: T) => any) | null = null
+    _convertForLoad: ((jsonVal: any) => T) | null = null
+
+    constructor(key: string, defaultVal: T, 
+                convertForSave: ((storage: T) => any) | null = null,
+                convertForLoad: ((jsonVal: any) => T) | null = null) {
+        this.key = key
+        this._convertForSave = convertForSave
+        this._convertForLoad = convertForLoad
+
+        var jsonVal = localStorage.getItem(key)
+        if (jsonVal != null) {
+            this._storage = JSON.parse(jsonVal)["data"]
+            if (this._storage == undefined) {
+                this._storage = defaultVal
+            }
+            else if (this._convertForLoad != null) {
+                this._storage = this._convertForLoad(this._storage)
+            }
+        }
+        else {
+            this._storage = defaultVal
+        }
+    }
+
+    get(): T {
+        return this._storage
+    }
+
+    save() {
+        var toStore: any
+        if (this._convertForSave != undefined) {
+            toStore = this._convertForSave(this._storage)
+        }
+        else {
+            toStore = this._storage
+        }
+        localStorage.setItem(this.key, JSON.stringify({'data': toStore}))
+    }
+    
+}
+
+export { StoredVal }
